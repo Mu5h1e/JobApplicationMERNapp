d7b895975871a8bbe37f7fd46446c6d1e4345245
Mu5h1e <mushiesidd@gmail.com>
2021-01-10 18:50:23 +0530

    basic app setup p2

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,31 @@ const morgan = require('morgan')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 
+const app = express()
+
+
 // Config .env to ./config/config.env
 require('dotenv').config({
 path:'./config/config.env'
 })
 
-const app = express()
+// Config for only development
+if (process.env.NODE_ENV === 'development') {
+    app.use(cors({
+        origin: process.env.CLIENT_URL
+    }))
+
+    app.use(morgan('dev'))
+    // Morgan provides information about each request
+    // cors helps deal with react for localhost without any issues
+}
+
+app.use((req,res,next) => {
+    res.status(404).json({
+        success: false,
+        message: 'Page not found',
+    })
+})
 
 const PORT = process.env.PORT
 
