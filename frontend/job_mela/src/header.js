0ac7cd99fc04e7c31f3369894785ea89c72e2633
Mu5h1e <mushiesidd@gmail.com>
2021-01-10 17:46:21 +0530

    basic app setup p1

---
+++ frontend/job_mela/src/header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './header.css'
+
+let Header = ()  => 
+    <div>
+        <div className='mainHeaderDiv'>
+            <h1>This is our header</h1>
+            <h1>This is 2</h1>
+        </div>
+    </div>
+
+export default Header
