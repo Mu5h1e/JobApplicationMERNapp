7b5710a92fbe04aefd3a6e2854980afe5ba00709
Mu5h1e <mushiesidd@gmail.com>
2021-01-12 00:20:29 +0530

    adding jwt and loadash

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,3 +1,6 @@
+const User = require('../models/auth.model')
+const expressJwt = require('express-jwt')
+
 exports.registerController = (req,res) => {
     const {email,password} = req.body
     console.log(email, password)
