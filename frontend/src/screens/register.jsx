1e4e8068a61e816e7a17332cf08873ecf662b2a6
Mu5h1e <mushiesidd@gmail.com>
2021-01-12 16:37:55 +0530

    implemented helper functions

---
+++ frontend/src/screens/register.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+import authSvg from '../assets/auth.svg'
+import {ToastContainer, toast} from 'react-toastify'
+import { authenticate, isAuth } from '../helpers/auth'
+import axios from 'axios'
+import {Redirect} from 'react-router-dom'
+
+const Register = () => {
+    return(
+        <div>
+            Register Page
+        </div>
+    )
+}
