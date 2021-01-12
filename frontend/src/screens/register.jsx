ab0533f243da0384d6e6f14248a102683aadc44d
Mu5h1e <mushiesidd@gmail.com>
2021-01-12 18:18:53 +0530

    handle submit and handle change register page

--- frontend/src/screens/register.jsx
+++ frontend/src/screens/register.jsx
@@ -6,9 +6,47 @@ import axios from 'axios'
 import {Redirect} from 'react-router-dom'
 
 const Register = () => {
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        password1: '',
+        password2: '',
+    })
+    const {name, email, password1, password2} = formData
+
+    const handleChange = text => e => {
+        setFormData({...formData, [text]: e.target.value})
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault()
+        if(name && email && password1) {
+            if(password1 === password2) {
+                axios.post(`${process.env.REACT_APP_API_URL}/register`, {
+                    name,email,password:password1
+                }).then(res => {
+                    setFormData({
+                        ...formData,
+                        name:'',
+                        email:'',
+                        password1:'',
+                        password2:''
+                    })
+                    toast.success(res.data.message)
+                }).catch(err=>{
+                    toast.error(err.response.data.error)
+                })
+            } else {
+                toast.error('Passwords do not match')
+            }
+        } else {
+            toast.error('Please fill all the fields')
+        }
+    }
+
     return(
-        <div>
-            Register Page
+        <div className='min-h-screen bg-grat-100 text-gray-900 flex justify-center'>
+            {isAuth() ? <Redirect to='/'/> : null}
         </div>
     )
 }
