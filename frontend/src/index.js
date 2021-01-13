be8986a27c7b7c9b6bd358ea45a77e884ac89c6b
Mu5h1e <mushiesidd@gmail.com>
2021-01-13 17:37:50 +0530

    linked activation to signup

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom';
 import {BrowserRouter, Route, Redirect, Switch} from 'react-router-dom'
 import App from './App';
 import Register from './screens/register'
+import Activate from './screens/activate'
 import 'react-toastify/dist/ReactToastify.css'
 ReactDOM.render(
   <BrowserRouter>
     <switch>
       <Route path='/' exact render={props => <App {...props}/>}/>
       <Route path='/register/' exact render={props => <Register {...props}/>}/>
+      <Route path='/users/activate/:token' exact render={props => <Activate {...props}/>}/>
     </switch>
   </BrowserRouter>,
   document.getElementById('root')
