245f71aa2d75fdb772ca0d628181a16a4d715c3d
Mu5h1e <mushiesidd@gmail.com>
2021-01-14 18:08:42 +0530

    error 404?

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -5,7 +5,8 @@ const {
     registerController,
     activationController,
     loginController,
-    forgotPasswordController
+    forgotPasswordController,
+    resetPasswordController
 } = require('../controllers/auth.controller.js')
 
 const {
@@ -19,4 +20,6 @@ router.post('/activation', activationController)
 router.post('/login', validLogin, loginController)
 router.post('/register', validSign, registerController)
 router.put('/forgotpassword', forgotPasswordValidator, forgotPasswordController);
+router.post('/resetpassword', resetPasswordValidator, resetPasswordController);
+
 module.exports = router
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,6 +6,7 @@ import Register from './screens/register'
 import Activate from './screens/activate'
 import Login from './screens/login'
 import ForgotPassword from './screens/forgotPassword.jsx';
+import ResetPassword from './screens/resetPassword.jsx';
 import 'react-toastify/dist/ReactToastify.css'
 ReactDOM.render(
   <BrowserRouter>
@@ -15,7 +16,7 @@ ReactDOM.render(
       <Route path='/login/' exact render={props => <Login {...props}/>}/>
       <Route path='/users/activate/:token' exact render={props => <Activate {...props}/>}/>
       <Route path='/users/password/forget' exact render={props => <ForgotPassword {...props} />} />
-
+      <Route path='/users/password/reset/:token' exact render={props => <ResetPassword {...props} />} />
     </Switch>
   </BrowserRouter>,
   document.getElementById('root')
