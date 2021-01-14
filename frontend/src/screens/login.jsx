102162056cffc9d5f25c343455dd20c7b22a1a5e
Mu5h1e <mushiesidd@gmail.com>
2021-01-14 15:48:36 +0530

    sign in not registering - issue fixed

--- frontend/src/screens/login.jsx
+++ frontend/src/screens/login.jsx
@@ -1,102 +1,124 @@
-import React, { useState } from 'react'
-import {ToastContainer, toast} from 'react-toastify'
-import { authenticate, isAuth } from '../helpers/auth.helper'
-import axios from 'axios'
-import {Redirect} from 'react-router-dom'
+import React, { useState } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import axios from 'axios';
+import { authenticate, isAuth } from '../helpers/auth.helper';
+import { Link, Redirect } from 'react-router-dom';
 
-const Login = () => {
-    const [formData, setFormData] = useState({
-        email: '',
-        password1: '',
-        textChange: 'Sign In'
-    })
-    const { email, password1, textChange} = formData
+const Login = ({ history }) => {
+  const [formData, setFormData] = useState({
+    email: '',
+    password1: '',
+    textChange: 'Sign In'
+  });
+  const { email, password1, textChange } = formData;
+  const handleChange = text => e => {
+    setFormData({ ...formData, [text]: e.target.value });
+  };
 
-    const handleChange = text => e => {
-        setFormData({...formData, [text]: e.target.value})
+  const handleSubmit = e => {
+    console.log(process.env.REACT_APP_API_URL);
+    e.preventDefault();
+    if (email && password1) {
+      setFormData({ ...formData, textChange: 'Submitting' });
+      axios
+        .post(`http://localhost:5000/api/login`, {
+          email,
+          password: password1
+        })
+        .then(res => {
+          authenticate(res, () => {
+            setFormData({
+              ...formData,
+              email: '',
+              password1: '',
+              textChange: 'Submitted'
+            });
+            isAuth() && isAuth().role === 'admin'
+              ? history.push('/admin')
+              : history.push('/private');
+            toast.success(`Hey ${res.data.user.name}, Welcome back!`);
+          });
+        })
+        .catch(err => {
+          setFormData({
+            ...formData,
+            email: '',
+            password1: '',
+            textChange: 'Sign In'
+          });
+          console.log(err.response);
+          toast.error(err.response.data.errors);
+        });
+    } else {
+      toast.error('Please fill all fields');
     }
-
-
-    const handleSubmit = e => {
-        e.preventDefault()
-        if(email && password1) {
-            axios.post(`http://localhost:5000/api/login`, {
-                email,password:password1
-            }).then(res => {
-                setFormData({
-                    ...formData,
-                    email:'',
-                    password1:'',
-                })
-                    toast.success(res.data.message)
-                }).catch(err=>{
-                    toast.error(err.response.data.error)
-                })
-        } else {
-            toast.error('Please fill all the fields')
-        }
-    }
-
-    return(
-        <div className='min-h-screen bg-gray-100 text-gray-900 flex justify-center'>
-        {isAuth() ? <Redirect to='/' /> : null}
-        <ToastContainer />
-        <div className='max-w-screen-xl m-0 sm:m-20 bg-white shadow sm:rounded-lg flex justify-center flex-1'>
-          <div className='lg:w-1/2 xl:w-5/12 p-6 sm:p-12'>
-            <div className='mt-12 flex flex-col items-center'>
-              <h1 className='text-2xl xl:text-3xl font-extrabold'>
-                Sign In to JobWala
-              </h1>
-  
+  };
+  return (
+    <div className='min-h-screen bg-gray-100 text-gray-900 flex justify-center'>
+      {isAuth() ? <Redirect to='/' /> : null}
+      <ToastContainer />
+      <div className='max-w-screen-xl m-0 sm:m-20 bg-white shadow sm:rounded-lg flex justify-center flex-1'>
+        <div className='lg:w-1/2 xl:w-5/12 p-6 sm:p-12'>
+          <div className='mt-12 flex flex-col items-center'>
+            <h1 className='text-2xl xl:text-3xl font-extrabold'>
+              Sign In for Congar
+            </h1>
+            <div className='w-full flex-1 mt-8 text-indigo-500'>
+              <div className='flex flex-col items-center'>
+                <a
+                  className='w-full max-w-xs font-bold shadow-sm rounded-lg py-3
+           bg-indigo-100 text-gray-800 flex items-center justify-center transition-all duration-300 ease-in-out focus:outline-none hover:shadow focus:shadow-sm focus:shadow-outline mt-5'
+                  href='/register'
+                  target='_self'
+                >
+                  <i className='fas fa-user-plus fa 1x w-6  -ml-2 text-indigo-500' />
+                  <span className='ml-4'>Sign Up</span>
+                </a>
+              </div>
+              <div className='my-12 border-b text-center'>
+                <div className='leading-none px-2 inline-block text-sm text-gray-600 tracking-wide font-medium bg-white transform translate-y-1/2'>
+                  Or sign In with e-mail
+                </div>
+              </div>
               <form
-                className='w-full flex-1 mt-8 text-indigo-500'
+                className='mx-auto max-w-xs relative '
                 onSubmit={handleSubmit}
               >
-                <div className='mx-auto max-w-xs relative '>
-                  <input
-                    className='w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5'
-                    type='email'
-                    placeholder='Email'
-                    onChange={handleChange('email')}
-                    value={email}
-                  />
-                  <input
-                    className='w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5'
-                    type='password'
-                    placeholder='Password'
-                    onChange={handleChange('password1')}
-                    value={password1}
-                  />              
-                  <button
-                    type='submit'
-                    className='mt-5 tracking-wide font-semibold bg-indigo-500 text-gray-100 w-full py-4 rounded-lg hover:bg-indigo-700 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none'
-                  >
-                    <i className='fas fa-user-plus fa 1x w-6  -ml-2' />
-                  </button>
-                </div>
-                <div className='my-12 border-b text-center'>
-                  <div className='leading-none px-2 inline-block text-sm text-gray-600 tracking-wide font-medium bg-white transform translate-y-1/2'>
-                    new? sign up
-                  </div>
-                </div>
-                <div className='flex flex-col items-center'>
-                  <a
-                    className='w-full max-w-xs font-bold shadow-sm rounded-lg py-3
-             bg-indigo-100 text-gray-800 flex items-center justify-center transition-all duration-300 ease-in-out focus:outline-none hover:shadow focus:shadow-sm focus:shadow-outline mt-5'
-                    href='/register'
-                    target='_self'
-                  >
-                    <i className='fas fa-sign-in-alt fa 1x w-6  -ml-2 text-indigo-500' />
-                    <span className='ml-4'>Sign Up</span>
-                  </a>
-                </div>
+                <input
+                  className='w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white'
+                  type='email'
+                  placeholder='Email'
+                  onChange={handleChange('email')}
+                  value={email}
+                />
+                <input
+                  className='w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5'
+                  type='password'
+                  placeholder='Password'
+                  onChange={handleChange('password1')}
+                  value={password1}
+                />
+                <button
+                  type='submit'
+                  className='mt-5 tracking-wide font-semibold bg-indigo-500 text-gray-100 w-full py-4 rounded-lg hover:bg-indigo-700 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none'
+                >
+                  <i className='fas fa-sign-in-alt  w-6  -ml-2' />
+                  <span className='ml-3'>Sign In</span>
+                </button>
+                <Link
+                  to='/users/password/forget'
+                  className='no-underline hover:underline text-indigo-500 text-md text-right absolute right-0  mt-2'
+                >
+                  Forget password?
+                </Link>
               </form>
             </div>
           </div>
         </div>
-        ;
       </div>
-    )
-}
+      ;
+    </div>
+  );
+};
 
-export default Login
+export default Login;
